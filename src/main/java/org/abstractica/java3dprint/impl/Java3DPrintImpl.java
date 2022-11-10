29b94159614f4bf1e534ac54911417a56c9f3397
tgrundtvig <TGrundtvig@gmail.com>
2022-11-10 14:11:15 +0100

    Added more constructors

--- src/main/java/org/abstractica/java3dprint/impl/Java3DPrintImpl.java
+++ src/main/java/org/abstractica/java3dprint/impl/Java3DPrintImpl.java
@@ -11,8 +11,24 @@ public class Java3DPrintImpl extends JavaCSGImpl implements Java3DPrint
 	private final Adjust solidTight;
 	private final Adjust solidLoose;
 
+	public Java3DPrintImpl(String moduleDirectoryName,
+	                       boolean binarySTL,
+	                       Adjust holeTight,
+	                       Adjust holeLoose,
+	                       Adjust solidTight,
+	                       Adjust solidLoose)
+	{
+		super(moduleDirectoryName, binarySTL);
+		this.holeTight = holeTight;
+		this.holeLoose = holeLoose;
+		this.solidTight = solidTight;
+		this.solidLoose = solidLoose;
+	}
+
 	public Java3DPrintImpl
 			(
+				String moduleDirectoryName,
+				boolean binarySTL,
 				double holeTightXYAdj,
 				double holeTightZAdj,
 				double holeLooseXYAdj,
@@ -23,15 +39,33 @@ public class Java3DPrintImpl extends JavaCSGImpl implements Java3DPrint
 				double solidLooseZAdj
 			)
 	{
-		this.holeTight = new AdjustImpl(holeTightXYAdj, holeTightZAdj);
-		this.holeLoose = new AdjustImpl(holeLooseXYAdj, holeLooseZAdj);
-		this.solidTight = new AdjustImpl(solidTightXYAdj, solidTightZAdj);
-		this.solidLoose = new AdjustImpl(solidLooseXYAdj, solidLooseZAdj);
+		this(   moduleDirectoryName, binarySTL,
+				new AdjustImpl(holeTightXYAdj, holeTightZAdj),
+				new AdjustImpl(holeLooseXYAdj, holeLooseZAdj),
+				new AdjustImpl(solidLooseXYAdj, solidLooseZAdj),
+				new AdjustImpl(solidLooseXYAdj, solidLooseZAdj));
+	}
+
+
+
+	public Java3DPrintImpl(String moduleDirectoryName,
+	                       boolean binarySTL)
+	{
+		this( moduleDirectoryName, binarySTL,
+				0.1,
+				0.1,
+				0.2,
+				0.2,
+				-0.1,
+				-0.1,
+				-0.2,
+				-0.2);
 	}
 
 	public Java3DPrintImpl()
 	{
-		this(   0.1,
+		this( null, false,
+				0.1,
 				0.1,
 				0.2,
 				0.2,
